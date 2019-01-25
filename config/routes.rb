03fee750d716408d9f9573b3afe82050c103bc78
Gustavo Chapim <gustavo.chapim@gmail.com>
2019-01-25 22:59:34 +0000

    Adding scope to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :people
-  resources :movies
+  scope '/api' do
+    resources :people
+    resources :movies
+  end
 end
